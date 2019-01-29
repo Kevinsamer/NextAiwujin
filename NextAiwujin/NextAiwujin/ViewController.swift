2aee49f2945a52abcbcb7929ed87623d469fba52
yzc <756988824@qq.com>
2019-01-29 12:29:50 +0800

    初始化

---
+++ NextAiwujin/NextAiwujin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NextAiwujin
+//
+//  Created by DEV2018 on 2019/1/29.
+//  Copyright © 2019 DEV2018. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
